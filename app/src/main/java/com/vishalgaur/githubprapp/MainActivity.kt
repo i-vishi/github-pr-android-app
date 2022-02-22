3ef18318f13f8a53e5cf55621edd62dce1c1afee
vishal_gaur <vishal1.gaur@paytm.com>
2022-02-22 21:46:40 +0530

    project init

---
+++ app/src/main/java/com/vishalgaur/githubprapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.vishalgaur.githubprapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vishalgaur.githubprapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
